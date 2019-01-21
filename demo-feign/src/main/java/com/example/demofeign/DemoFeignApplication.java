81ff22b02e8a9ea09ade6f080ca0478a4e885474
wxb820901@gmail.com <wxb820901@gmail.com>
2019-01-21 16:46:36 +0800

    20190121_update_fetch_for_test_r1r2

--- demo-feign/src/main/java/com/example/demofeign/DemoFeignApplication.java
+++ demo-feign/src/main/java/com/example/demofeign/DemoFeignApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@RestController
 public class DemoFeignApplication {
 
 	@Autowired
@@ -27,9 +30,12 @@ public class DemoFeignApplication {
 	public void init() {
 		System.out.println("====>"+customersClient.getCustomers());
 		System.out.println("====>"+customersClient.getCustomer(1l));
-		System.out.println("====>"+env.getActiveProfiles().length);
-
 		System.out.println("====>"+customersClient.getConfigMsg());
+	}
+	
+	@GetMapping("/configMsg" )
+	public String configMsg(){
+		return customersClient.getConfigMsg();//r1 or r2
 	}
 
 }
