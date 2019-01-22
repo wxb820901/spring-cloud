c639b7a48195e1b57e7527505743be9383466747
wxb820901@gmail.com <wxb820901@gmail.com>
2019-01-22 10:17:44 +0800

    20190122_add_jks_in_rest_repo_for_decrypt

--- demo-feign/src/main/java/com/example/demofeign/DemoFeignApplication.java
+++ demo-feign/src/main/java/com/example/demofeign/DemoFeignApplication.java
@@ -3,11 +3,14 @@ package com.example.demofeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import javax.annotation.PostConstruct;
 
@@ -15,6 +18,7 @@ import javax.annotation.PostConstruct;
 @EnableFeignClients
 @EnableDiscoveryClient
 @RestController
+@EnableCircuitBreaker
 public class DemoFeignApplication {
 
 	@Autowired
@@ -33,10 +37,14 @@ public class DemoFeignApplication {
 		System.out.println("====>"+customersClient.getConfigMsg());
 	}
 	
+	@HystrixCommand(fallbackMethod = "reliable")
 	@GetMapping("/configMsg" )
 	public String configMsg(){
 		return customersClient.getConfigMsg();//r1 or r2
 	}
+	public String reliable() {
+	    return "Cloud Native Java (O'Reilly)";
+}
 
 }
 
