e55b69f2daf90f3a0401277a016798f05166872c
wxb820901@gmail.com <wxb820901@gmail.com>
2019-02-25 14:32:33 +0800

    20190225_add_webflux

---
+++ demo-webflux/src/main/java/com/example/demowebflux/DemoWebfluxApplication.java
@@ -0,0 +1,34 @@
+package com.example.demowebflux;
+
+import com.example.demorestrepo.entity.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/Customers")
+public class DemoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebfluxApplication.class, args);
+	}
+
+	@GetMapping("/{id}")
+	private Mono<Customer> getCustomerById(@PathVariable String id) {
+		return Mono.just(new Customer("firstName","lastName"));
+	}
+
+	@GetMapping
+	private Flux<Customer> getAllEmployees() {
+		return Flux.fromIterable(Arrays.asList(new Customer("AkkName1","AllName1"), new Customer("AkkName2","AllName2")));
+	}
+
+}
