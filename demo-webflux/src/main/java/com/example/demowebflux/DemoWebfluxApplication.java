ea2f6c487c12aa6132778fd5127a73d1c93d8eed
wxb820901@gmail.com <wxb820901@gmail.com>
2019-02-28 15:11:03 +0800

    20190228_refactor_webflux

--- demo-webflux/src/main/java/com/example/demowebflux/DemoWebfluxApplication.java
+++ demo-webflux/src/main/java/com/example/demowebflux/DemoWebfluxApplication.java
@@ -3,6 +3,7 @@ package com.example.demowebflux;
 import com.example.demorestrepo.entity.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 @SpringBootApplication
 @RestController
 @RequestMapping("/Customers")
+@EnableDiscoveryClient
 public class DemoWebfluxApplication {
 
 	public static void main(String[] args) {
