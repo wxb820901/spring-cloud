444c3c57712111c650f9d9008528f8b9ae7ecd1b
wxb820901@gmail.com <wxb820901@gmail.com>
2019-06-11 12:03:20 +0800

    20190611_add_mongo

--- demo-mongodb/src/test/java/com/example/demomongodb/AbstractIntegrationTest.java
+++ demo-mongodb/src/test/java/com/example/demomongodb/AbstractIntegrationTest.java
@@ -44,69 +44,6 @@ public class AbstractIntegrationTest {
         System.out.println("mongoTemplate ==> "+mongoTemplate);
         System.out.println("mongo ==> "+mongo);
 
-//        // Customers
-//
-//        DBCollection customers = mongo.getDB("mydatabase").getCollection("customer");
-//        customers.remove(new BasicDBObject());
-//
-//        BasicDBObject address = new BasicDBObject();
-//        address.put("city", "New York");
-//        address.put("street", "Broadway");
-//        address.put("country", "United States");
-//
-//        BasicDBList addresses = new BasicDBList();
-//        addresses.add(address);
-//
-//        DBObject dave = new BasicDBObject("firstname", "Dave");
-//        dave.put("lastname", "Matthews");
-//        dave.put("email", "dave@dmband.com");
-//        dave.put("addresses", addresses);
-//
-//        customers.insert(dave);
-//
-//        // Products
-//
-//        DBCollection products = database.getCollection("product");
-//        products.drop();
-//
-//        DBObject iPad = new BasicDBObject("name", "iPad");
-//        iPad.put("description", "Apple tablet device");
-//        iPad.put("price", 499.0);
-//        iPad.put("attributes", new BasicDBObject("connector", "plug"));
-//
-//        DBObject macBook = new BasicDBObject("name", "MacBook Pro");
-//        macBook.put("description", "Apple notebook");
-//        macBook.put("price", 1299.0);
-//
-//        BasicDBObject dock = new BasicDBObject("name", "Dock");
-//        dock.put("description", "Dock for iPhone/iPad");
-//        dock.put("price", 49.0);
-//        dock.put("attributes", new BasicDBObject("connector", "plug"));
-//
-//        products.insert(iPad, macBook, dock);
-//
-//        // Orders
-//
-//        DBCollection orders = database.getCollection("order");
-//        orders.drop();
-//
-//        // Line items
-//
-//        DBObject iPadLineItem = new BasicDBObject("product", iPad);
-//        iPadLineItem.put("amount", 2);
-//
-//        DBObject macBookLineItem = new BasicDBObject("product", macBook);
-//        macBookLineItem.put("amount", 1);
-//
-//        BasicDBList lineItems = new BasicDBList();
-//        lineItems.add(iPadLineItem);
-//        lineItems.add(macBookLineItem);
-//
-//        DBObject order = new BasicDBObject("customer", new DBRef(database.getName(), "customer", dave.get("_id")));
-//        order.put("lineItems", lineItems);
-//        order.put("shippingAddress", address);
-//
-//        orders.insert(order);
     }
 
     @Test
--- demo-mongodb/src/test/java/com/example/demomongodb/ApplicationConfig.java
+++ demo-mongodb/src/test/java/com/example/demomongodb/ApplicationConfig.java
@@ -1,15 +1,9 @@
 package com.example.demomongodb;
 import java.io.IOException;
 import java.util.List;
-
-import javax.sql.DataSource;
-
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
 import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
 import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
@@ -19,10 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.transaction.PlatformTransactionManager;
-
 import com.mongodb.Mongo;
 import com.mongodb.WriteConcern;
 
@@ -35,39 +26,29 @@ public class ApplicationConfig extends AbstractMongoConfiguration {
     private List<Converter<?, ?>> converters;
 
     public Mongo mongo() throws Exception {
-
         Mongo mongo = new Mongo();
         mongo.setWriteConcern(WriteConcern.SAFE);
-
         return mongo;
     }
 
     private static final MongodStarter starter = MongodStarter.getDefaultInstance();
-//    private MongoClient mongo;
-//    private DB db;
-//    private MongodProcess mongod;
     @Override
     public MongoClient mongoClient() {
         MongoClient mongo = null;
         try {
             MongodExecutable mongodExecutable = starter.prepare(new MongodConfigBuilder() .version(Version.Main.PRODUCTION)
                     .net(new Net(12345, Network.localhostIsIPv6())).build());
-
             mongodExecutable.start();
             mongo = new MongoClient("localhost", 12345);
-//            db = mongo.getDB("mydatabase");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
         return mongo;
     }
 
     @Override
     protected String getDatabaseName() {
-        return "e-store";
+        return "mydatabase";
     }
 
 
