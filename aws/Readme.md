4148dce26450a3429b98d16d5f63fdd3e710a7f9
wxb820901@gmail.com <wxb820901@gmail.com>
2019-06-18 16:25:15 +0800

    20190618_add_aws

---
+++ aws/Readme.md
@@ -0,0 +1,21 @@
+AWS learning account: wxb820901awslearn@126.com
+
+1, IAM
+
+create user：  admin/m6EF_!qKNfDCrGR
+create group for grant: admins
+
+https://211226149717.signin.aws.amazon.com/console
+访问密钥 ID: AKIATCLQPINK3K2G2Q7K
+私有访问密钥: vtjolKHirLRrdGpkcTzfWXJCOWG3EirDWKCFc3Ij
+
+
+2, S3
+
+create bucket: 	aws-demo-20190617
+				aws-demo-20190617-2
+
+
+3, EC2
+
+putty login ec2-user@ec2-18-221-132-29.us-east-2.compute.amazonaws.com with key1privatekey.ppk
---
+++ aws/s3/src/main/java/com/aws/demo/DemoAWSApplication.java
@@ -0,0 +1,36 @@
+package com.aws.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@EnableContextInstanceData
+public class DemoAWSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAWSApplication.class, args);
+
+    }
+    @Value("${ami-id:N/A}")
+    private String amiId;
+
+    @Value("${hostname:N/A}")
+    private String hostname;
+
+    @Value("${instance-type:N/A}")
+    private String instanceType;
+
+    @Value("${services/domain:N/A}")
+    private String serviceDomain;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("init amiId ===> "+amiId);
+        System.out.println("init hostname ===> "+hostname);
+        System.out.println("init instanceType ===> "+instanceType);
+        System.out.println("init serviceDomain ===> "+serviceDomain);
+    }
+}
