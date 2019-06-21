d8389dac856ce3522bd4bd55d99d65f2fda5f838
bill_wang <bill_wang@epam.com>
2019-06-21 15:06:56 +0800

    20190621_continue_aws

--- aws/dydb/src/test/java/com/aws/demo/AmazonDynamoDBTest.java
+++ aws/dydb/src/test/java/com/aws/demo/AmazonDynamoDBTest.java
@@ -2,6 +2,7 @@ package com.aws.demo;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,6 +15,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
@@ -27,47 +29,52 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 
 public class AmazonDynamoDBTest {
-    @Test
-    public void test() throws IOException {
+    AmazonDynamoDB client;
+    public static final String tableName = "Movies";
+    public static DynamoDB dynamoDB;
+    @BeforeClass
+    public static void before() throws IOException {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.US_WEST_2)
                 .build();
 
-//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-//                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
-//                .build();
-        DynamoDB dynamoDB = new DynamoDB(client);
-//create table
-        String tableName = "Movies";
+        dynamoDB = new DynamoDB(client);
+        Table table = dynamoDB.getTable(tableName);
 
         try {
             System.out.println("Attempting to create table; please wait...");
-            Table table = dynamoDB.createTable(tableName,
-                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
-                            // key
+
+            table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition key
                             new KeySchemaElement("title", KeyType.RANGE)), // Sort key
                     Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
                             new AttributeDefinition("title", ScalarAttributeType.S)),
                     new ProvisionedThroughput(10L, 10L));
             table.waitForActive();
-            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
         } catch (Exception e) {
             System.err.println("Unable to create table: ");
             System.err.println(e.getMessage());
         }
 
-
+    }
+    @Test
+    public void testLoadFromJson() throws IOException {
         //load data
-        Table table = dynamoDB.getTable("Movies");
+        Table table = dynamoDB.getTable(tableName);
         JsonParser parser = new JsonFactory().createParser(new File("C:\\Users\\bill_wang\\spring-cloud\\aws\\dydb\\src\\test\\resources\\moviedata.json"));
         JsonNode rootNode = new ObjectMapper().readTree(parser);
         Iterator<JsonNode> iter = rootNode.iterator();
         JsonNode currentNode;
-
 
         currentNode = iter.next();
         int year = currentNode.asInt();
@@ -77,10 +84,8 @@ public class AmazonDynamoDBTest {
         currentNode = iter.next();
         String info = currentNode.toString();
         try {
-
             table.putItem(new Item().withPrimaryKey("year", year, "title", title).withJSON("info",
                     info));
-            System.out.println("PutItem succeeded: " + year + " " + title);
         } catch (Exception e) {
             System.err.println("Unable to add movie: " + year + " " + title);
             System.err.println(e.getMessage());
@@ -88,39 +93,45 @@ public class AmazonDynamoDBTest {
         }
 
         parser.close();
-
-
-        //add and query
-        year = 2015;
-        title = "The Big New Movie";
-
+        GetItemSpec spec = new GetItemSpec().withPrimaryKey("year", year, "title", title);
+        Item result = queryByPK(spec);
+        assertEquals(result.get("year"),new BigDecimal(2013));
+        assertEquals(result.get("title"), "Turn It Down, Or Else!");
+    }
+    @Test
+    public void testPutAndDelete(){
+        int year = 2015;
+        String title = "The Big New Movie";
+        Table table = dynamoDB.getTable(tableName);
         final Map<String, Object> infoMap = new HashMap<String, Object>();
         infoMap.put("plot", "Nothing happens at all.");
         infoMap.put("rating", 0);
-
         try {
-            System.out.println("Adding a new item...");
             PutItemOutcome outcome = table
                     .putItem(new Item().withPrimaryKey("year", year, "title", title).withMap("info", infoMap));
-
-            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
-
         } catch (Exception e) {
             System.err.println("Unable to add item: " + year + " " + title);
             System.err.println(e.getMessage());
         }
-
-        //query
         GetItemSpec spec = new GetItemSpec().withPrimaryKey("year", year, "title", title);
+        Item result = queryByPK(spec);
 
-        try {
-            System.out.println("Attempting to read the item...");
-            Item outcome = table.getItem(spec);
-            System.out.println("GetItem succeeded: " + outcome);
+        assertEquals(result.get("year"),new BigDecimal(2015));
+        assertEquals(result.get("title"), "The Big New Movie");
+        DeleteItemSpec delSpec = new DeleteItemSpec().withPrimaryKey("year", year, "title", title);
+        table.deleteItem(delSpec);
+        result = queryByPK(spec);
+        assertNull(result);
 
-        } catch (Exception e) {
-            System.err.println("Unable to read item: " + year + " " + title);
-            System.err.println(e.getMessage());
-        }
+    }
+
+    private Item queryByPK(GetItemSpec spec){
+        Table table = dynamoDB.getTable(tableName);
+        return table.getItem(spec);
+    }
+    @AfterClass
+    public static void after() throws IOException {
+        Table table = dynamoDB.getTable(tableName);
+        table.delete();
     }
 }
--- aws/sns/src/test/java/com/aws/demo/SNSTest.java
+++ aws/sns/src/test/java/com/aws/demo/SNSTest.java
@@ -3,95 +3,64 @@ package com.aws.demo;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class SNSTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-    //SNS create add subscribe and publish msg
-    @Test
-    public void test(){
-        AmazonSNS snsClient = AmazonSNSClient.builder()
+
+public class SNSTest {
+    public static final AmazonSNS sns = AmazonSNSClient.builder()
                 .withRegion("us-west-1")
                 .build();
-        // Create an Amazon SNS topic.
-        final CreateTopicRequest createTopicRequest = new CreateTopicRequest("MyTopic");
-        final CreateTopicResult createTopicResult = snsClient.createTopic(createTopicRequest);
+    public static String topicArn;
+    public static final String topicName = "major-topic";
+    @BeforeClass
+    public static void beforeClass(){
 
-        // Print the topic ARN.
-        System.out.println("TopicArn:" + createTopicResult.getTopicArn());
+    }
 
-        // Print the request ID for the CreateTopicRequest action.
-        System.out.println("CreateTopicRequest: " + snsClient.getCachedResponseMetadata(createTopicRequest));
-
-
-        //list SNS
-        final ListTagsForResourceRequest listTagsForResourceRequest = new ListTagsForResourceRequest();
-        listTagsForResourceRequest.setResourceArn(createTopicResult.getTopicArn());
-        final ListTagsForResourceResult listTagsForResourceResult = snsClient.listTagsForResource(listTagsForResourceRequest);
-        System.out.println(String.format("ListTagsForResource: \tTags for topic %s are %s.\n",
-                createTopicResult.getTopicArn(), listTagsForResourceResult.getTags()));
+    @Test
+    public void testAddTopicPutMsgDeleteTopic(){
 
 
-//        //tag
-//        final Tag tagTeam = new Tag();
-//        tagTeam.setKey("Team");
-//        tagTeam.setValue("Development");
-//        final Tag tagEnvironment = new Tag();
-//        tagEnvironment.setKey("Environment");
-//        tagEnvironment.setValue("Gamma");
-//
-//        final List<Tag> tagList = new ArrayList<>();
-//        tagList.add(tagTeam);
-//        tagList.add(tagEnvironment);
-//
-//        final TagResourceRequest tagResourceRequest = new TagResourceRequest();
-//        tagResourceRequest.setResourceArn(createTopicResult.getTopicArn());
-//        tagResourceRequest.setTags(tagList);
-//        final TagResourceResult tagResourceResult = snsClient.tagResource(tagResourceRequest);
-//
-//        System.out.println("tagResourceResult ==> " + tagResourceResult);
-//
-//        //untag
-//        final UntagResourceRequest untagResourceRequest = new UntagResourceRequest();
-//        untagResourceRequest.setResourceArn(createTopicResult.getTopicArn());
-//        final List<String> tagKeyList = new ArrayList<>();
-//        tagKeyList.add("Team");
-//        untagResourceRequest.setTagKeys(tagKeyList);
-//        final UntagResourceResult untagResourceResult = snsClient.untagResource(untagResourceRequest);
-//
-//        System.out.println("untagResourceResult ==> " + untagResourceResult);
+        CreateTopicRequest create = new CreateTopicRequest()
+                .withName("major-topic");
+        CreateTopicResult createResult = sns.createTopic(create);
+        topicArn = createResult.getTopicArn();
+        assertNotNull("verify returned topic ARN", topicArn);
 
+        ListTopicsResult listTopicResult = sns.listTopics();
+        assertEquals("after insert topic list should contain 1 item",1,listTopicResult.getTopics().size());
+        assertEquals("after insert topic list should contain before inserted topic arn", topicArn,
+                listTopicResult.getTopics().get(0).getTopicArn());
 
+        SubscribeRequest subscribeRequest = new SubscribeRequest(topicArn, "email", "bill_wang@epam.com");
+        sns.subscribe(subscribeRequest);
 
-        // Subscribe an email endpoint to an Amazon SNS topic.
-        SubscribeRequest subscribeRequest = new SubscribeRequest(createTopicResult.getTopicArn(), "email", "bill_wang@epam.com");
-        snsClient.subscribe(subscribeRequest);
-
-//        // Print the request ID for the SubscribeRequest action.
-//        System.out.println("SubscribeRequest: " + snsClient.getCachedResponseMetadata(subscribeRequest));
-//        System.out.println("To confirm the subscription, check your email.");
-
-        // Publish a message to an Amazon SNS topic.
         String msg = "If you receive this message, publishing a message to an Amazon SNS topic works.";
-        PublishRequest publishRequest = new PublishRequest(createTopicResult.getTopicArn(),"email", msg);
-        PublishResult publishResponse = snsClient.publish(publishRequest);
+        PublishRequest publishRequest = new PublishRequest(topicArn,"email", msg);
+        PublishResult publishResult = sns.publish(publishRequest);
 
-//        // Print the MessageId of the message.
-//        System.out.println("MessageId: " + publishResponse.getMessageId());
-//        System.out.println("SdkResponseMetadata: " + publishResponse.getSdkResponseMetadata());
+//
+//
+//        DeleteTopicResult deleteResult = sns.deleteTopic(topicArn);
+//        assertNotNull("verify returned delete result", deleteResult);
 
-
-        // Delete an Amazon SNS topic.
-        final DeleteTopicRequest deleteTopicRequest = new DeleteTopicRequest(createTopicResult.getTopicArn());
-        snsClient.deleteTopic(deleteTopicRequest);
-//        // Print the request ID for the DeleteTopicRequest action.
-//        System.out.println("DeleteTopicRequest: " + snsClient.getCachedResponseMetadata(deleteTopicRequest));
-
-
-
+//        ListTopicsResult listTopicsAfterDeletion = sns.listTopics();
+//        assertEquals("topic list should contain zero items after deletion",0,listTopicsAfterDeletion.getTopics().size());
+    }
+    @AfterClass
+    public static void afterClass(){
+        for(Topic topic : sns.listTopics().getTopics()) {
+            DeleteTopicRequest deleteTopicRequest = new DeleteTopicRequest(topic.getTopicArn());
+            sns.deleteTopic(deleteTopicRequest);
+        }
     }
 }
