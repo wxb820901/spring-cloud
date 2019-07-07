be707ad6b956f76241d53ca10b8ce38372dd02c9
wxb820901@gmail.com <wxb820901@gmail.com>
2019-07-08 00:21:59 +0800

    20190707_add_localstack_lambda_apigateway_sh_and_case

--- aws/aws-lambda/src/main/java/com/aws/demo/HelloAPIGateway.java
+++ aws/aws-lambda/src/main/java/com/aws/demo/HelloAPIGateway.java
@@ -2,22 +2,69 @@ package com.aws.demo;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 
 public class HelloAPIGateway implements RequestStreamHandler {
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//        StringBuilder sb = new StringBuilder();
+//        String str = null;
+//        while((str = reader.readLine()) != null){
+//            sb.append(str);
+//        }
+//
+//        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+//        writer.write("hello api gateway request begin");
+//        writer.write(sb.toString());
+//        writer.write("hello api gateway request end");
+//        writer.close();
+        handleGetByParam(inputStream, outputStream, context);
+
+    }
+
+    public void handleGetByParam(
+            InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+
+        JSONParser parser = new JSONParser();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        StringBuilder sb = new StringBuilder();
-        String str = null;
-        while((str = reader.readLine()) != null){
-            sb.append(str);
+        JSONObject responseJson = new JSONObject();
+
+
+        try {
+            JSONObject event = (JSONObject) parser.parse(reader);
+            JSONObject responseBody = new JSONObject();
+            System.out.println("=====>event=" + event);
+            if (event.get("pathParameters") != null) {
+                JSONObject pps = (JSONObject) event.get("pathParameters");
+                if (pps.get("id") != null) {
+                    int id = Integer.parseInt((String) pps.get("id"));
+                    System.out.println("=====>id=" + id);
+                }
+            }
+
+
+            responseBody.put("message", "No item found");
+            responseJson.put("statusCode", 404);
+
+
+            JSONObject headerJson = new JSONObject();
+            headerJson.put("x-custom-header", "my custom header value");
+
+            responseJson.put("headers", headerJson);
+            responseJson.put("body", responseBody.toString());
+
+        } catch (ParseException pex) {
+            responseJson.put("statusCode", 400);
+            responseJson.put("exception", pex);
         }
 
         OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-        writer.write("hello api gateway request begin");
-        writer.write(sb.toString());
-        writer.write("hello api gateway request end");
+        writer.write(responseJson.toString());
         writer.close();
     }
 }
